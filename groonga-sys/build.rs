aa9c1ed5c6f447fc1ef7307945c81c3d28ab5578
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-11-02 01:18:04 +0900

    Windows でビルドできるようにビルドスクリプトを変更した。

--- groonga-sys/build.rs
+++ groonga-sys/build.rs
@@ -1,21 +1,37 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let libgroonga = pkg_config::probe_library("groonga").unwrap();
-    let include_path = libgroonga.include_paths.first().unwrap();
+    let include_path = match pkg_config::probe_library("groonga") {
+        Ok(lib) => lib.include_paths.first().unwrap().clone(),
+        _ => {
+            let home = env::var("GROONGA_HOME").unwrap();
+            let home = Path::new(&home);
 
-    println!("cargo:rerun-if-changed=src/macro.h");
-    println!("cargo:rerun-if-changed=src/macro.c");
+            println!("cargo:rustc-link-lib=libgroonga");
+            println!(
+                "cargo:rustc-link-search=native={}",
+                home.join("lib").display()
+            );
+
+            home.join("include").join("groonga")
+        }
+    };
+
+    let macro_header = Path::new("src").join("macro.h");
+    let macro_src = Path::new("src").join("macro.c");
+
+    println!("cargo:rerun-if-changed={}", macro_header.display());
+    println!("cargo:rerun-if-changed={}", macro_src.display());
 
     cc::Build::new()
-        .file("src/macro.c")
-        .include(include_path)
+        .file(macro_src.to_str().unwrap())
+        .include(&include_path)
         .compile("macro");
 
     let bindings = bindgen::Builder::default()
         .header(include_path.join("groonga.h").to_str().unwrap())
-        .header("src/macro.h")
+        .header(macro_header.to_str().unwrap())
         .clang_arg(format!("-I{}", include_path.display()))
         .rustified_enum(".*")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
